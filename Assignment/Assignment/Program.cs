11162f7807b345a29309ebd3c33ca69d5e0a6beb
김정원 <akino0205@naver.com>
2022-05-23 23:43:47 +0900

    백준 문제 <조건문> 2753 윤년 14681 사분면 고르기 2525 오븐 시계 2480 주사위 세개 <반복문> 2741 N찍기 2742 기찍 N 11021 A+B - 7 11022 A+B - 8 2438 별 찍기 - 1 <문자열> 11654 아스키 코드 11720 숫자의 합

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -1,15 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections;
+using System.Text;
+#region Console write
 Console.WriteLine("================ Select Number! ================");
 Console.WriteLine("1.백준 14719 빗물");
-Console.WriteLine("2.백준 9498 시험 성적");
-Console.WriteLine("3.백준 2753 윤년");
+Console.WriteLine("================     조건문     ================");
+Console.WriteLine("2-1.백준 9498 시험 성적");
+Console.WriteLine("2-2.백준 2753 윤년");
+Console.WriteLine("2-3.백준 14681 사분면고르기");
+Console.WriteLine("2-4.백준 2525 오븐시계");
+Console.WriteLine("2-5.백준 2480 주사위 세개");
+Console.WriteLine("================     반복문     ================");
+Console.WriteLine("3-1.백준 2741 N찍기");
+Console.WriteLine("3-2.백준 2742 기찍 N");
+Console.WriteLine("3-3.백준 11021 A+B - 7");
+Console.WriteLine("3-4.백준 11022 A+B - 8");
+Console.WriteLine("3-5.백준 2438 별찍기 - 1");
+Console.WriteLine("================     문자열     ================");
+Console.WriteLine("4-1.백준 11654 아스키 코드");
+Console.WriteLine("4-2.백준 11720 숫자의 합");
+#endregion Console write
+
 switch (Console.ReadLine())
 {
     case "1": RainWaterSum(); break;
-    case "2": ExamResult(); break;
-    case "3": IsLeapYear(); break;
+    case "2-1": ExamResult(); break;
+    case "2-2": IsLeapYear(); break;
+    case "2-3": WhichQuadrant(); break;
+    case "2-4": OvenWatch(); break;
+    case "2-5": ThreeDice(); break;
+    case "3-1": WriteN(); break;
+    case "3-2": WriteNReverse(); break;
+    case "3-3": WriteSum7(); break;
+    case "3-4": WriteSum8(); break;
+    case "3-5": WriteStars(); break;
+    case "4-1": AsciiCode(); break;
+    case "4-2": SumNumbers(); break;
     default: break;
 }
 /// <summary>
@@ -39,7 +66,7 @@ void RainWaterSum()
 
     Console.WriteLine(rainwater);
 }
-
+#region 조건문
 /// <summary>
 /// 백준 9498 시험 성적 : 
 /// 시험 점수를 입력받아
@@ -68,7 +95,6 @@ void ExamResult()
 
     Console.WriteLine(grade);
 }
-
 /// <summary>
 /// 백준 2753 윤년 : 
 /// 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
@@ -86,4 +112,188 @@ void IsLeapYear()
     Console.WriteLine(isLeapYear ? "1" : "0");
 
 }
+/// <summary>
+/// 백준 14681 사분면고르기
+/// : 점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오. 단, x좌표와 y좌표는 모두 양수나 음수라고 가정한다.
+/// </summary>
+void WhichQuadrant()
+{
+    int x = int.Parse(Console.ReadLine());
+    int y = int.Parse(Console.ReadLine());
+    int quadrant = 0;
+    if (x > 0 )
+    {
+        if (y > 0) quadrant = 1;
+        else quadrant = 4;
+    }
+    else
+    {
+        if (y > 0) quadrant = 2;
+        else quadrant = 3;
+    }
+    Console.WriteLine(quadrant);
+}
+/// <summary>
+/// 백준 2525 오븐시계
+/// : 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때, 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
+/// 입력 : 첫째 줄에는 현재 시각이 나온다. 현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다. 두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다. 
+/// 출력 : 첫째 줄에 종료되는 시각의 시와 분을 공백을 사이에 두고 출력한다. (단, 시는 0부터 23까지의 정수, 분은 0부터 59까지의 정수이다. 디지털 시계는 23시 59분에서 1분이 지나면 0시 0분이 된다.)
+/// </summary>
+void OvenWatch()
+{
+    string[] timeStr = Console.ReadLine().Split();
+    int hourNow = int.Parse(timeStr[0]);
+    int minuteNow = int.Parse(timeStr[1]);
+    int cookingMinute = int.Parse(Console.ReadLine());
+    minuteNow += cookingMinute;
+
+    int finishMinute = minuteNow % 60;
+    int finishHour = hourNow + minuteNow / 60;
+
+    Console.WriteLine($"{finishHour%24} {finishMinute}");
+}
+/// <summary>
+/// 백준 2480 주사위 세개
+/// : 1에서부터 6까지의 눈을 가진 3개의 주사위를 던져서 다음과 같은 규칙에 따라 상금을 받는 게임이 있다. 
+/// 1. 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다. 
+/// 2. 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다. 
+/// 3. 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.  
+/// </summary>
+void ThreeDice()
+{
+    string[] temp = Console.ReadLine().Split();
+    int dice1 = int.Parse(temp[0]);
+    int dice2 = int.Parse(temp[1]);
+    int dice3 = int.Parse(temp[2]);
+    int reward;
+    if (dice1 == dice2 && dice2 == dice3)
+        reward = 10000 + dice1 * 1000;
+    else if (dice1 == dice2 || dice1 == dice3 || dice2 == dice3)
+    {
+        int spots = dice1 == dice2 || dice1 == dice3 ? dice1 : dice2;
+        reward = 1000 + spots * 100;
+    }
+    else
+    {
+        int max = dice1 > dice2 ? dice1 : dice2;
+        max = max > dice3 ? max : dice3;
+        reward = max * 100;
+    }
+    Console.WriteLine(reward);
+}
+#endregion 조건문
+#region 반복문
+/// <summary>
+/// 백준 2741 N찍기
+/// : 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+/// 시간 제한 1초, 메모리 제한 128MB
+/// </summary>
+void WriteN()
+{
+    int inputN = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i <= inputN; i++)
+    {
+        sb.AppendLine(i.ToString());
+    }
+    Console.Write(sb.ToString());
+}
+/// <summary>
+/// 백준 2742 기찍 N
+/// : 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+/// 시간 제한 1초, 메모리 제한 128MB
+/// </summary>
+void WriteNReverse()
+{
+    int inputN = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = inputN; i > 0; i-- )
+    {
+        sb.AppendLine(i.ToString());
+    }
+    Console.Write(sb.ToString());
+}
+/// <summary>
+/// 백준 11021 A+B - 7
+///: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+/// 입력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+///시간 제한 1초, 메모리 제한 256MB
+/// </summary>
+void WriteSum7()
+{
+    int count = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i <= count; i++)
+    {
+        string[] num = Console.ReadLine().Split();
+        sb.AppendLine($"Case #{i}: {int.Parse(num[0]) + int.Parse(num[1])}");
+    }
+    Console.Write(sb.ToString());
+}
+/// <summary>
+/// 백준 11022 A+B - 8
+///: 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+/// 입력 : 첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+///시간 제한 1초, 메모리 제한 256MB
+/// </summary>
+void WriteSum8()
+{
+    int count = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= count; i++)
+    {
+        string[] num = Console.ReadLine().Split();
+        sb.AppendLine($"Case #{i}: {num[0]} + {num[1]} = {int.Parse(num[0]) + int.Parse(num[1])}");
+    }
+    Console.Write(sb.ToString());
+}
+/// <summary>
+/// 3-5.백준 2438 별찍기 - 1
+/// : 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+/// 시간 제한 1초 메모리제한 128MB
+/// </summary>
+void WriteStars()
+{
+    int inputN = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i <= inputN; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            sb.Append("*");
+        }
+        sb.Append("\n");
+    }
+    Console.WriteLine(sb.ToString());
+}
+#endregion 반복문
+#region 문자열
+/// <summary>
+/// 백준 11654 아스키 코드
+/// : 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+/// </summary>
+void AsciiCode()
+{
+    char inputChar = char.Parse(Console.ReadLine());
+    Console.Write((byte)inputChar);
+}
+/// <summary>
+/// 백준 11720 숫자의 합
+/// : N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+/// 입력 : 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+/// </summary>
+void SumNumbers()
+{
+    int count = int.Parse(Console.ReadLine());
+    var input = Console.ReadLine().ToArray();
+    int result = 0;
+    foreach(char numChar in input)
+    {
+        result += int.Parse(numChar.ToString());
+    }
+    Console.Write(result);
+}
+#endregion 문자열
+
+
 
